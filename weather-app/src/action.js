d78d9c5669ffad73c065ce3ffaadfc9c3131ed8f
sai milind <sai9615@gmail.com>
2020-02-26 19:02:06 -0500

    Created weather app

---
+++ weather-app/src/action.js
@@ -0,0 +1,70 @@
+import xhr from 'xhr';
+
+export function getData(url) {
+    return function thunk(dispatch) {
+      xhr({
+        url: url
+      }, function (err, data) {
+  
+        var body = JSON.parse(data.body);
+        var list = body.list;
+        var dates = [];
+        var temps = [];
+        for (var i = 0; i < list.length; i++) {
+          dates.push(list[i].dt_txt);
+          temps.push(list[i].main.temp);
+        }
+  
+        dispatch(setData(body));
+        dispatch(setDates(dates));
+        dispatch(setTemps(temps));
+        dispatch(setSelectedDate(''));
+        dispatch(setSelectedTemp(null));
+      });
+    }
+}
+
+export function changeLocation(location)
+{
+    return{
+        type: 'CHANGE_LOCATION',
+        location: location
+    }
+}
+
+export function setSelectedTemp(temp)
+{
+    return{
+        type: 'CHANGE_SELECTED_TEMPERATURE',
+        temp: temp
+    }
+}
+
+export function setSelectedDate(date)
+{
+    return{
+        type: 'CHANGE_SELECTED_DATE',
+        date: date
+    }
+}
+
+export function setData(data) {
+    return {
+      type: 'SET_DATA',
+      data: data
+    };
+  }
+  
+  export function setDates(dates) {
+    return {
+      type: 'SET_DATES',
+      dates: dates
+    };
+  }
+  
+  export function setTemps(temps) {
+    return {
+      type: 'SET_TEMPS',
+      temps: temps
+    };
+  }
---
+++ weather-app/src/reducer.js
@@ -0,0 +1,35 @@
+import {fromJS} from 'immutable'
+var initialState = fromJS({
+    location: '',
+    data: {},
+    dates: [],
+    temps: [],
+    selected: {
+      date: '',
+      temp: null
+    }
+  })
+
+export default function Reducer(state = initialState, action){
+    switch(action.type){
+        case 'CHANGE_LOCATION' :
+            return state.set('location', action.location);
+
+        case 'SET_DATA':
+            return state.set('data', fromJS(action.data));
+
+        case 'SET_DATES':
+            return state.set('dates', fromJS(action.dates));
+
+        case 'SET_TEMPS':
+            return state.set('temps', fromJS(action.temps)); 
+
+        case 'CHANGE_SELECTED_TEMPERATURE' : 
+            return state.setIn(['selected', 'temp'], action.temp);
+
+        case 'CHANGE_SELECTED_DATE' :      
+            return state.setIn(['selected', 'date'], action.date);        
+
+        default : return state;
+        }
+}
